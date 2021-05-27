52f155d0ae1342eeff3b0c0d420b1238f2cff1cb
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-05-27 17:39:33 +0100

    Update kawasaki.h

--- kawasaki.h
+++ kawasaki.h
@@ -56,13 +56,13 @@ extern "C" {
     buf[17u] = 0x03u; \
     sprintf((char*)A,"%u%s",(char*)&buf,KAWA_CONNECTION_STRING_TUPLE); } while(0)
 #define KAWA_STATE_STRING_PARTIAL "wh\r\n     JT1       JT2       JT3       JT4       JT5       JT6  \r\n"
-#define KAWA_STATE_STRING_PARTIAL_LEN 60u
-#define KAWA_MAX_POSE_STATE_REPLY 60u
-#define KAWA_MAX_JOINT_STATE_REPLY 60u
+#define KAWA_STATE_STRING_PARTIAL_LEN 66u
+#define KAWA_MAX_POSE_STATE_REPLY 66u
+#define KAWA_MAX_JOINT_STATE_REPLY 66u
 #define KAWA_MOTOR_OFF_ERROR "(P1000)Cannot execute program because motor power is OFF."
 #define KAWA_TEACH_LOCK_ON_ERROR "(P1002)Cannot execute program because teach lock is ON."
 #define KAWA_TEACH_MODE_ERROR "(P1001)Cannot execute program in TEACH mode."
-#define KAWA_ERROR_MODE_ERROR = "(P1013)Cannot execute because in error now. Reset error."
+#define KAWA_ERROR_MODE_ERROR "(P1013)Cannot execute because in error now. Reset error."
 #define KAWA_ALREADY_RUNNING_ERROR "(P1009)Program is already running."
 #define KAWA_CANNOT_REACH_ERROR "(P1013)Cannot execute because in error now. Reset error."
 #define KAWA_NO_ERROR_FEEDBACK ">"
@@ -100,7 +100,32 @@ extern "C" {
 #define KAWA_completed_move_pub_topic "completed_move"
 #define KAWA_pose_sub_topic "command/pose"
 
-typedef enum { KAWA_RCV_START = 1, KAWA_SND_LOGIN = 2, KAWA_RCV_LOGIN = 3, KAWA_SND_GETSTATE = 4, KAWA_RCV_GETSTATE = 5, KAWA_RCV_POSE = 6, KAWA_NUM_MSG_STATES } KAWA_Msg_States_e;
+typedef enum {
+    KAWA_IDLE,
+    KAWA_OPEN_SOCKET,
+    KAWA_SOCKET_LARGE_WAIT,
+    KAWA_RCV_START,
+    KAWA_LOGIN_MEDIUM_WAIT,
+    KAWA_SND_LOGIN,
+    KAWA_TIMEOUT_LARGE_WAIT,
+    KAWA_RCV_LOGIN,
+    KAWA_SND_GETSTATE,
+    KAWA_GETSTATE_MEDIUM_WAIT,
+    KAWA_RCV_GETSTATE,
+    KAWA_RCV_POSE,
+    KAWA_SND_POSE,
+    KAWA_TIMEOUT_MEDIUM_WAIT,
+    KAWA_RCV_SND_POSE,
+    KAWA_NUM_MSG_STATES 
+} KAWA_Msg_States_e;
+
+/* list of error states sits in alarm state until acknowledge */
+#define KAWA_SET_ERROR_SET_MTR_OFF 1u + KAWA_NUM_MSG_STATES
+#define KAWA_SET_ERROR_TEACH_LOCK_ON_ERROR 2u + KAWA_NUM_MSG_STATES
+#define KAWA_SET_ERROR_ERROR_MODE_ERROR 3u + KAWA_NUM_MSG_STATES
+#define KAWA_SET_ERROR_ALREADY_RUNNING_ERROR 4u + KAWA_NUM_MSG_STATES
+#define KAWA_SET_ERROR_CANNOT_REACH_ERROR 5u + KAWA_NUM_MSG_STATES
+#define KAWA_SET_ERROR_TEACH_MODE_ERROR 6u + KAWA_NUM_MSG_STATES
 
 #if defined(D_FT900)
 typedef struct KAWAPACKED {
@@ -134,28 +159,62 @@ typedef struct  {
 }) KAWA_joint_t;                                                                
 #endif
 
+#define KAWA_MAX_FEEDBACK_CMD_REPLY 100u
+#define KAWA_MAX_FEEDBACK_ERROR_REPLY 100u
+
+#define KAWA_SOCKET_CLOSED 0u
+#define KAWA_SOCKET_OPEN_SUCESS 1u
+
 #if defined(D_FT900)
 typedef struct KAWAPACKED {
-    KAWA_Msg_States_e state;                                                    /* state of message collection system */
-    uint16_t socket_number;                                                     /* socket number when socket opened */
-    KAWA_joint_t joint_states;                                                  /* information about joint */
-    KAWA_pose_t pose_states;                                                    /* information about pose */
+    KAWA_Msg_States_e state;                                                     /* state of message collection system */
+    uint16_t rcv_socket_number;                                                  /* socket number when socket opened */
+    uint16_t snd_socket_number;                                                  /* socket number when socket opened */
+    uint8_t socketState;
+    KAWA_joint_t joint_states;                                                   /* information about joint */
+    KAWA_pose_t pose_states;                                                     /* information about pose */
+    char setCmd[KAWA_MAX_FEEDBACK_CMD_REPLY];
+    char feedbackCmd[KAWA_MAX_FEEDBACK_CMD_REPLY];
+    char feedbackError[KAWA_MAX_FEEDBACK_ERROR_REPLY];
 } kawasaki_robot_t;
 #else
 KAWAPACKED(
 typedef struct  {
-    KAWA_Msg_States_e state;                                                    /* state of message collection system */
-    uint16_t socket_number;                                                     /* socket number when socket opened */
-    KAWA_joint_t joint_states;                                                  /* information about joint */
-    KAWA_pose_t pose_states;                                                    /* information about pose */
+    KAWA_Msg_States_e state;                                                     /* state of message collection system */
+    uint16_t rcv_socket_number;                                                  /* socket number when socket opened */
+    uint16_t snd_socket_number;                                                  /* socket number when socket opened */
+    uint8_t socketState;
+    KAWA_joint_t joint_states;                                                   /* information about joint */
+    KAWA_pose_t pose_states;                                                     /* information about pose */
+    char setCmd[KAWA_MAX_FEEDBACK_CMD_REPLY];
+    char feedbackCmd[KAWA_MAX_FEEDBACK_CMD_REPLY];
+    char feedbackError[KAWA_MAX_FEEDBACK_ERROR_REPLY];
 }) kawasaki_robot_t;                                                                
 #endif
 
+#define KAWA_SET_POSE_SEND(A,rob)  do{ sprintf(A,"b DO LMOVE TRANS(%3.3f,%3.3f,%3.3f,%3.3f,%3.3f,%3.3f)\n",rob->pose_states.position.x,rob->pose_states.position.y,rob->pose_states.position.z,rob->pose_states.orientation_deg.x,rob->pose_states.orientation_deg.y,rob->pose_states.orientation_deg.z); } while(0)
+#define KAWA_SET_POSE_MSG_SIZE 100u
+/* some compilers didnt like the macro passing a pointer - solutions are use one of these */
+#define KAWA_SET_POSE_SEND2(A,rob)  do{ sprintf(A,"b DO LMOVE TRANS(%3.3f,%3.3f,%3.3f,%3.3f,%3.3f,%3.3f)\n",rob.pose_states.position.x,rob.pose_states.position.y,rob.pose_states.position.z,rob.pose_states.orientation_deg.x,rob.pose_states.orientation_deg.y,rob.pose_states.orientation_deg.z); } while(0)
+void KAWA_set_pose_sendfunc(const char * A, kawasaki_robot_t *rob)  
+{ 
+#if (MCU_NAME == PIC32MX795F512L) /* different protype in pic32 C */
+    sprintf((char*)A,"b DO LMOVE TRANS(%3.3f,%3.3f,%3.3f,%3.3f,%3.3f,%3.3f)\n",rob->pose_states.position.x,rob->pose_states.position.y,rob->pose_states.position.z,rob->pose_states.orientation_deg.x,rob->pose_states.orientation_deg.y,rob->pose_states.orientation_deg.z); 
+#else
+    sprintf(A,"b DO LMOVE TRANS(%3.3f,%3.3f,%3.3f,%3.3f,%3.3f,%3.3f)\n",rob->pose_states.position.x,rob->pose_states.position.y,rob->pose_states.position.z,rob->pose_states.orientation_deg.x,rob->pose_states.orientation_deg.y,rob->pose_states.orientation_deg.z);         
+#endif
+}  
+
+/* =================== external functions for use =========================== */
 extern uint8_t kawasakiGetJoint(kawasaki_robot_t *rob);
 extern uint8_t kawasakiGetPose(kawasaki_robot_t *rob);
 extern uint8_t kawasakiParseState( const char* msg, kawasaki_robot_t *rob );
-extern void kawasakiRcvSendmsg( const char * rcv, char * send, kawasaki_robot_t *rob );
-
+extern uint8_t kawasakiSetPose( kawasaki_robot_t *rob, const dVectr position, const dquat quaternion, char * msg );
+extern uint8_t kawasakiSetPose2( kawasaki_robot_t *rob, const dVectr position, const dquat quaternion, char * msg );
+extern uint8_t kawasakiParseSetFeedback( char* msg, kawasaki_robot_t *rob );
+extern void kawasakiRcvSendmsg( const char * rcv, char * send, kawasaki_robot_t *rob, const dVectr posit, const dquat quat );
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* kawasaki robot end */
